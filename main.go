9063e71c6f4c42ca5c641e674c7dc6179bc54862
bianyuanop <chen.me.nan@gmail.com>
2024-04-01 16:16:30 -0400

    1mb data uploading

--- main.go
+++ main.go
@@ -24,40 +24,10 @@ func main() {
 	ctx := context.Background()
 
 	var wg sync.WaitGroup
-	wg.Add(3)
+	wg.Add(1)
 
 	// Submitting simultaneously
 	fmt.Println("submitting data1")
-
-	// go func() {
-	// 	defer wg.Done()
-	// 	size1 := 0.5 * 1024 * 1024
-	// 	err = submitWithSize(ctx, cli, int(size1))
-	// 	if err != nil {
-	// 		fmt.Printf("unable to submit with size %d, err: %s\n", int(size1), err)
-	// 	}
-	// }()
-
-	// go func() {
-	// 	defer wg.Done()
-	// 	size1 := 1 * 1024 * 1024
-	// 	err = submitWithSize(ctx, cli, int(size1))
-	// 	if err != nil {
-	// 		fmt.Printf("unable to submit with size %d, err: %s\n", int(size1), err)
-	// 	}
-	// }()
-
-	// time.Sleep(5 * time.Second)
-
-	go func() {
-		defer wg.Done()
-		size1 := 0.15 * 1024 * 1024
-		err = submitWithSize(ctx, cli, int(size1))
-		if err != nil {
-			fmt.Printf("unable to submit with size %d, err: %s\n", int(size1), err)
-		}
-	}()
-	time.Sleep(3 * time.Second)
 
 	go func() {
 		defer wg.Done()
@@ -68,28 +38,7 @@ func main() {
 		}
 	}()
 
-	time.Sleep(3 * time.Second)
-	go func() {
-		defer wg.Done()
-		size1 := 0.1 * 1024 * 1024
-		err = submitWithSize(ctx, cli, int(size1))
-		if err != nil {
-			fmt.Printf("unable to submit with size %d, err: %s\n", int(size1), err)
-		}
-	}()
 	wg.Wait()
-	// data := make([]byte, int(size))
-	// _, err = rand.Read(data)
-	// if err != nil {
-	// 	fmt.Println("unable to gen random data1")
-	// }
-	// ctx, cancle := context.WithTimeout(ctx, 70*time.Second)
-	// defer cancle()
-	// h, err := cli.SubmitData(ctx, data)
-	// if err != nil {
-	// 	fmt.Printf("unable to submit data1, reason: %s\n", err.Error())
-	// }
-	// fmt.Printf("hash of tx1: %s\n", h.Hex())
 }
 
 func submitWithSize(ctx context.Context, cli *client.AvailClient, size int) error {
